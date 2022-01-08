05c68571f253cbe16b840073d23cd59e6324f189
Eugen <eugen.rascanu88@gmail.com>
2022-01-08 15:27:00 +0000

    Add Btn's + modal in CSS

--- script.js
+++ script.js
@@ -4,49 +4,49 @@ var AllQuestions = [{
     b: 'Yippee-ki-yay!',
     c: 'Shoot the glass!',
     d: 'Easy now, cowboy!',
-    answer: 'ansb'
+    answer: 'ans2'
 }, {
     question: "How many prime directives did 'Robocop' have in the first original movie?",
     a: '4',
     b: '6',
     c: '3',
     d: '10',
-    answer: 'ansa'
+    answer: 'ans1'
 }, {
     question: "In 'Predator 2', after killing the City Hunter, Lieutenant Mike Harrigan receives a flintlock pistol. What year was engraved on it?",
     a: '1840',
     b: '1675',
     c: '1910',
     d: '1715',
-    answer: 'ansd'
+    answer: 'ans4'
 }, {
     question: "In the first 'Termiantor' movie, the T-800, played by Arnold Schwarzenegger, had what mission?",
     a: 'Protect John Connor',
     b: 'Eliminate Sarah Connor',
     c: 'Protect SkyNet',
     d: 'Eliminate mankind',
-    answer: 'ansb'
+    answer: 'ans2'
 }, {
     question: "In 'Star Wars: Revenge of the Sith', the fighting scene between Anakin Skywalker and Obi Wan Kenobi, who had higher ground?",
     a: 'Anakin Skywalker',
     b: 'Obi Wan Kenobi',
     c: 'Both',
     d: 'Neither',
-    answer: 'ansb'
+    answer: 'ans2'
 }, {
     question: "What was the name of the first film in the 'Harry Potter' series?",
     a: 'Harry Potter and the Order of the Phoenix',
     b: 'Harry Potter and the Prisoner of Azkaban',
     c: 'Harry Potter and the Goblet of Fire',
     d: "Harry Potter And The Philosopher's Stone",
-    answer: 'ansd'
+    answer: 'ans4'
 }, {
     question: "In 'Beverly Hills Cop', Axel Foley was a cop from what city",
     a: 'Chicago',
     b: 'Beverly Hills',
     c: 'Detroit',
     d: 'New York',
-    answer: 'ansc'
+    answer: 'ans3'
 }, {
     question: "In what year was 'Rush Hour' produced?",
     a: '1998',
@@ -60,14 +60,14 @@ var AllQuestions = [{
     b: 'Django Unchained',
     c: 'The Revenant',
     d: 'Titanic',
-    answer: 'ansc'
+    answer: 'ans3'
 }, {
     question: "In 'Back To The Future', how many miles per hour did the DeLorean had to reach in order to time-travel?",
     a: '100',
     b: '88',
     c: '75',
     d: '90',
-    answer: 'ansb'
+    answer: 'ans2'
 }, ];
 
 
@@ -196,7 +196,7 @@ submitAnswerBtn.addEventListener('click', () => {
         resultScoreMarkup.innerHTML = `${score}/${AllQuestions.length}`;
         resultsContent.style.display = "block";
     } else {
-        setTimeout(nextQuestion, 400);
+        setTimeout(nextQuestion, 1000);
     }
 
     modal.style.display = "block";
