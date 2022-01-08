d87092c404d35fc7995a900f63ac924d24e74a82
Eugen <eugen.rascanu88@gmail.com>
2022-01-08 20:48:19 +0000

    Create JS folder. Add Questions. Modify HTML.

---
+++ assets/js/questions.js
@@ -0,0 +1,71 @@
+let allQuestions = [{
+    question: "In the hit movie 'Die Hard', what is John MClane's famous catch-phrase?",
+    a: 'Ho-Ho-Ho, now i have a machinegun!',
+    b: 'Yippee-ki-yay!',
+    c: 'Shoot the glass!',
+    d: 'Easy now, cowboy!',
+    answer: 'ans2'
+}, {
+    question: "How many prime directives did 'Robocop' have in the first original movie?",
+    a: '4',
+    b: '6',
+    c: '3',
+    d: '10',
+    answer: 'ans1'
+}, {
+    question: "In 'Predator 2', after killing the City Hunter, Lieutenant Mike Harrigan receives a flintlock pistol. What year was engraved on it?",
+    a: '1840',
+    b: '1675',
+    c: '1910',
+    d: '1715',
+    answer: 'ans4'
+}, {
+    question: "In the first 'Termiantor' movie, the T-800, played by Arnold Schwarzenegger, had what mission?",
+    a: 'Protect John Connor',
+    b: 'Eliminate Sarah Connor',
+    c: 'Protect SkyNet',
+    d: 'Eliminate mankind',
+    answer: 'ans2'
+}, {
+    question: "In 'Star Wars: Revenge of the Sith', the fighting scene between Anakin Skywalker and Obi Wan Kenobi, who had higher ground?",
+    a: 'Anakin Skywalker',
+    b: 'Obi Wan Kenobi',
+    c: 'Both',
+    d: 'Neither',
+    answer: 'ans2'
+}, {
+    question: "What was the name of the first film in the 'Harry Potter' series?",
+    a: 'Harry Potter and the Order of the Phoenix',
+    b: 'Harry Potter and the Prisoner of Azkaban',
+    c: 'Harry Potter and the Goblet of Fire',
+    d: "Harry Potter And The Philosopher's Stone",
+    answer: 'ans4'
+}, {
+    question: "In 'Beverly Hills Cop', Axel Foley was a cop from what city",
+    a: 'Chicago',
+    b: 'Beverly Hills',
+    c: 'Detroit',
+    d: 'New York',
+    answer: 'ans3'
+}, {
+    question: "In what year was 'Rush Hour' produced?",
+    a: '1998',
+    b: '1999',
+    c: '2000',
+    d: '2001',
+    answer: 'ans1'
+}, {
+    question: "Leonardo DiCaprio won his first Oscar for which movie?",
+    a: 'The Wolf Of Wall Street',
+    b: 'Django Unchained',
+    c: 'The Revenant',
+    d: 'Titanic',
+    answer: 'ans3'
+}, {
+    question: "In 'Back To The Future', how many miles per hour did the DeLorean had to reach in order to time-travel?",
+    a: '100',
+    b: '88',
+    c: '75',
+    d: '90',
+    answer: 'ans2'
+}, ];
