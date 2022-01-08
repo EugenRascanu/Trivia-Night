0fcdcfbd7dd4a36c6b00fdf9bb00db54cf067159
Eugen <eugen.rascanu88@gmail.com>
2022-01-08 20:52:37 +0000

    Modify script.js : moved questions to separate JS file, modify variable names to camelCase, add consistency to code, move all the global variable declarations to the top.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,46 @@
-function shuffleArray(array) {
-    const dummyArray = array.slice();
-    for (let i = dummyArray.length - 1; i > 0; i--) {
-        const rand = Math.floor(Math.random() * (i + 1));
-        [dummyArray[i], dummyArray[rand]] = [dummyArray[rand], dummyArray[i]];
+let selectedQuestions = [];
+let quizLength = 5;
 
-    }
-    return dummyArray;
-}
-allQuestions = shuffleArray(allQuestions);
-var mainDiv = document.querySelector('.mainDiv');
-var container = document.querySelector('.container');
-var question = document.querySelector('.question');
-var option1 = document.querySelector('#option1');
-var option2 = document.querySelector('#option2');
-var option3 = document.querySelector('#option3');
-var option4 = document.querySelector('#option4');
+let mainDiv = document.querySelector(".mainDiv");
+let container = document.querySelector(".container");
+let question = document.querySelector(".question");
+let option1 = document.querySelector("#option1");
+let option2 = document.querySelector("#option2");
+let option3 = document.querySelector("#option3");
+let option4 = document.querySelector("#option4");
 
-var submitAnswerBtn = document.querySelector('.submitAnswerBtn');
-var restartBtn = document.querySelector('.restartBtn');
+let submitAnswerBtn = document.querySelector(".submitAnswerBtn");
+let restartBtn = document.querySelector(".restartBtn");
 
-var userBtn = document.querySelector(".userBtn");
-var userInput = document.getElementById("user");
-var currentQuestionMarkup = document.querySelector('.currentQuestion');
-var answers = document.querySelectorAll('.answer');
+let userBtn = document.querySelector(".userBtn");
+let userInput = document.getElementById("user");
+let currentQuestionMarkup = document.querySelector(".currentQuestion");
+let answers = document.querySelectorAll(".answer");
 
-var score = 0,
+let score = 0,
     questionNumber = 0;
 
 //Modal elements
-var modal = document.getElementById("myModal");
-var modalContent = document.getElementById("content");
+let modal = document.getElementById("myModal");
+let modalContent = document.getElementById("content");
 
-var userNameForm = document.querySelector('.userNameForm');
-var rightAnswer = document.querySelector('.rightAnswer');
-var wrongAnswer = document.querySelector('.wrongAnswer');
-var noAnswer = document.querySelector('.noAnswer');
-var resultsContent = document.querySelector('.resultsContent');
-var restartQuizContent = document.querySelector('.restartQuizContent');
+let userNameForm = document.querySelector(".userNameForm");
+let rightAnswer = document.querySelector(".rightAnswer");
+let wrongAnswer = document.querySelector(".wrongAnswer");
+let noAnswer = document.querySelector(".noAnswer");
+let resultsContent = document.querySelector(".resultsContent");
+let restartQuizContent = document.querySelector(".restartQuizContent");
 
-var resultScoreMarkup = document.querySelector('.resultScore');
-var playAgain = document.querySelector('.playAgain');
-var confirmRestartBtn = document.querySelector('.confirmRestartBtn');
-var cancelRestartBtn = document.querySelector('.cancelRestartBtn');
-
-
-const getAnswer = () => {
-    var answer = -1;
-
-    answers.forEach(ans => {
-        if (ans.checked)
-            answer = ans.id;
-    });
-
-    return answer;
-};
-
-const nextQuestion = () => {
-    closeModal();
-    ++questionNumber;
-
-    answers.forEach(ans => ans.checked = false);
-    if (questionNumber < allQuestions.length)
-        loadQuestions();
-    else {
-        currentQuestionMarkup.innerHTML = `Question: ${questionNumber + 1}/${allQuestions.length}`;
-        localStorage.removeItem("userNameValue");
-        allQuestions = shuffleArray(allQuestions);
-    }
-}
-
-const loadQuestions = () => {
-    currentQuestionMarkup.innerHTML = `Question: ${questionNumber + 1}/${allQuestions.length}`;
-    var currentQuestion = allQuestions[questionNumber];
-
-    question.innerHTML = currentQuestion.question;
-    option1.innerHTML = currentQuestion.a;
-    option2.innerHTML = currentQuestion.b;
-    option3.innerHTML = currentQuestion.c;
-    option4.innerHTML = currentQuestion.d;
-};
-
-loadQuestions();
+let resultScoreMarkup = document.querySelector(".resultScore");
+let playAgain = document.querySelector(".playAgain");
+let confirmRestartBtn = document.querySelector(".confirmRestartBtn");
+let cancelRestartBtn = document.querySelector(".cancelRestartBtn");
 
 
-const closeModal = () => {
-    modal.style.display = "none";
-    userNameForm.style.display = "none";
-    rightAnswer.style.display = "none";
-    wrongAnswer.style.display = "none";
-    resultsContent.style.display = "none";
-    restartQuizContent.style.display = "none";
-    noAnswer.style.display = "none";
-}
-
-if (localStorage.getItem("userNameValue") == "" || !localStorage.getItem("userNameValue")) {
-    modal.style.display = "block";
-    userNameForm.style.display = "block";
-}
-
-//Events
-submitAnswerBtn.addEventListener('click', () => {
-    var markedAnswer = getAnswer();
-    var currentQuestion = allQuestions[questionNumber];
+//Event listener registrations
+submitAnswerBtn.addEventListener("click", () => {
+    let markedAnswer = getAnswer();
+    let currentQuestion = selectedQuestions[questionNumber];
 
     if (markedAnswer != -1) {
         if (markedAnswer == currentQuestion.answer) {
@@ -119,8 +58,8 @@ submitAnswerBtn.addEventListener('click', () => {
         return;
     }
 
-    if (questionNumber == allQuestions.length - 1) {
-        resultScoreMarkup.innerHTML = `${score}/${allQuestions.length}`;
+    if (questionNumber == selectedQuestions.length - 1) {
+        resultScoreMarkup.innerHTML = `${score}/${selectedQuestions.length}`;
         resultsContent.style.display = "block";
     } else {
         setTimeout(nextQuestion, 1000);
@@ -129,24 +68,24 @@ submitAnswerBtn.addEventListener('click', () => {
     modal.style.display = "block";
 });
 
-restartBtn.addEventListener('click', () => {
+restartBtn.addEventListener("click", () => {
     restartQuizContent.style.display = "block";
     modal.style.display = "block";
 });
 
-confirmRestartBtn.addEventListener('click', () => {
-    allQuestions = shuffleArray(allQuestions);
+confirmRestartBtn.addEventListener("click", () => {
+    selectedQuestions = shuffleArray(selectedQuestions);
     score = 0;
     questionNumber = 0;
     loadQuestions();
     closeModal();
 });
 
-cancelRestartBtn.addEventListener('click', () => {
+cancelRestartBtn.addEventListener("click", () => {
     closeModal();
 });
 
-userBtn.addEventListener('click', () => {
+userBtn.addEventListener("click", () => {
     console.log("user", userInput.value);
     let value = userInput.value;
     if (value != "") {
@@ -156,3 +95,85 @@ userBtn.addEventListener('click', () => {
         alert("Please Enter the user name");
     }
 })
+
+//Function definitions
+/**
+ * @param {Array} itemList - the array to be shuffled
+ * @returns {Array} resultArray
+ */
+function shuffleArray(itemList) {
+    const resultArray = itemList.slice();
+    for (let i = resultArray.length - 1; i > 0; i--) {
+        const rand = Math.floor(Math.random() * (i + 1));
+        [resultArray[i], resultArray[rand]] = [resultArray[rand], resultArray[i]];
+
+    }
+    return resultArray;
+}
+
+/**
+ * @returns {undefined} 
+ */
+function loadQuestions() {
+    currentQuestionMarkup.innerHTML = `Question: ${questionNumber + 1}/${selectedQuestions.length}`;
+    let currentQuestion = selectedQuestions[questionNumber];
+
+    question.innerHTML = currentQuestion.question;
+    option1.innerHTML = currentQuestion.a;
+    option2.innerHTML = currentQuestion.b;
+    option3.innerHTML = currentQuestion.c;
+    option4.innerHTML = currentQuestion.d;
+};
+
+/**
+ * @returns {string} answer
+ */
+function getAnswer() {
+    let answer = -1;
+
+    answers.forEach(ans => {
+        if (ans.checked)
+            answer = ans.id;
+    });
+
+    return answer;
+};
+
+/**
+ * @returns {undefined} 
+ */
+function nextQuestion() {
+    closeModal();
+    ++questionNumber;
+
+    answers.forEach(ans => ans.checked = false);
+    if (questionNumber < selectedQuestions.length)
+        loadQuestions();
+    else {
+        currentQuestionMarkup.innerHTML = `Question: ${questionNumber + 1}/${selectedQuestions.length}`;
+        localStorage.removeItem("userNameValue");
+        selectedQuestions = shuffleArray(selectedQuestions);
+    }
+}
+
+/**
+ * @returns {undefined} 
+ */
+function closeModal() {
+    modal.style.display = "none";
+    userNameForm.style.display = "none";
+    rightAnswer.style.display = "none";
+    wrongAnswer.style.display = "none";
+    resultsContent.style.display = "none";
+    restartQuizContent.style.display = "none";
+    noAnswer.style.display = "none";
+}
+
+allQuestions = shuffleArray(allQuestions);
+selectedQuestions = allQuestions.slice(0, quizLength);
+loadQuestions();
+
+if (localStorage.getItem("userNameValue") == "" || !localStorage.getItem("userNameValue")) {
+    modal.style.display = "block";
+    userNameForm.style.display = "block";
+}
