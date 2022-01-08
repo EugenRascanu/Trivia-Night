6ef0c63659fd1e8bccb4c315a45592a756f7d737
Eugen <eugen.rascanu88@gmail.com>
2022-01-08 22:01:31 +0000

    Add more questions to JS

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -27,7 +27,7 @@ let allQuestions = [{
     d: 'Eliminate mankind',
     answer: 'ans2'
 }, {
-    question: "In 'Star Wars: Revenge of the Sith', the fighting scene between Anakin Skywalker and Obi Wan Kenobi, who had higher ground?",
+    question: "In 'Star Wars Episode III: Revenge of the Sith', in the duel scene between Anakin Skywalker and Obi Wan Kenobi, who had higher ground?",
     a: 'Anakin Skywalker',
     b: 'Obi Wan Kenobi',
     c: 'Both',
@@ -133,10 +133,10 @@ let allQuestions = [{
     answer: 'ans3'
 }, {
     question: "In 'Back To The Future', how many miles per hour did the DeLorean had to reach in order to time-travel?",
-    a: '100',
-    b: '88',
-    c: '75',
-    d: '90',
+    a: '100 mph',
+    b: '88 mph',
+    c: '75 mph',
+    d: '90 mph',
     answer: 'ans2'
 }, {
     question: "Where were The Lord of the Rings movies filmed?",
@@ -279,73 +279,143 @@ let allQuestions = [{
     d: 'Steve Martin',
     answer: 'ans3'
 }, {
-    question: "In the hit movie 'Die Hard', what is John MClane's famous catch-phrase?",
-    a: 'Ho-Ho-Ho, now i have a machinegun!',
-    b: 'Yippee-ki-yay!',
-    c: 'Shoot the glass!',
-    d: 'Easy now, cowboy!',
+    question: "Which movie does not star Adam Sandler and Drew Barrymore?",
+    a: 'The Wedding Singer',
+    b: 'Fever Pitch',
+    c: 'Blended',
+    d: '50 First Dates',
     answer: 'ans2'
 }, {
-    question: "How many prime directives did 'Robocop' have in the first original movie?",
-    a: '4',
-    b: '6',
-    c: '3',
-    d: '10',
-    answer: 'ans1'
-}, {
-    question: "In 'Predator 2', after killing the City Hunter, Lieutenant Mike Harrigan receives a flintlock pistol. What year was engraved on it?",
-    a: '1840',
-    b: '1675',
-    c: '1910',
-    d: '1715',
-    answer: 'ans4'
-}, {
-    question: "In the first 'Termiantor' movie, the T-800, played by Arnold Schwarzenegger, had what mission?",
-    a: 'Protect John Connor',
-    b: 'Eliminate Sarah Connor',
-    c: 'Protect SkyNet',
-    d: 'Eliminate mankind',
-    answer: 'ans2'
-}, {
-    question: "In 'Star Wars: Revenge of the Sith', the fighting scene between Anakin Skywalker and Obi Wan Kenobi, who had higher ground?",
-    a: 'Anakin Skywalker',
-    b: 'Obi Wan Kenobi',
-    c: 'Both',
-    d: 'Neither',
-    answer: 'ans2'
-}, {
-    question: "What was the name of the first film in the 'Harry Potter' series?",
-    a: 'Harry Potter and the Order of the Phoenix',
-    b: 'Harry Potter and the Prisoner of Azkaban',
-    c: 'Harry Potter and the Goblet of Fire',
-    d: "Harry Potter And The Philosopher's Stone",
-    answer: 'ans4'
-}, {
-    question: "In 'Beverly Hills Cop', Axel Foley was a cop from what city",
-    a: 'Chicago',
-    b: 'Beverly Hills',
-    c: 'Detroit',
-    d: 'New York',
+    question: "In 'Passengers', Jennifer Lawrence and Chris Pratt star as voyagers to a new planet who awaken decades too early in their trip. Why does Lawrence’s character wake up?",
+    a: 'Her hibernation pod malfunctions',
+    b: 'The captain of the ship wakes her up so she can participate in an experiment with Pratt’s character',
+    c: 'Pratt’s character wakes her up because he wants a companion',
+    d: 'She has a medical emergency',
     answer: 'ans3'
 }, {
-    question: "In what year was 'Rush Hour' produced?",
-    a: '1998',
-    b: '1999',
-    c: '2000',
-    d: '2001',
-    answer: 'ans1'
-}, {
-    question: "Leonardo DiCaprio won his first Oscar for which movie?",
-    a: 'The Wolf Of Wall Street',
-    b: 'Django Unchained',
-    c: 'The Revenant',
-    d: 'Titanic',
+    question: "Around how long did it take to make The Nightmare Before Christmas?",
+    a: '2 years',
+    b: '5 years',
+    c: '3 years',
+    d: '6 years',
     answer: 'ans3'
 }, {
-    question: "In 'Back To The Future', how many miles per hour did the DeLorean had to reach in order to time-travel?",
-    a: '100',
-    b: '88',
-    c: '75',
-    d: '90',
+    question: "What’s the name of the planet Obi-Wan Kenobi and Anakin Skywalker duel on in 'Star Wars Episode III: Revenge of the Sith?'",
+    a: 'Mustafar',
+    b: 'Hoth',
+    c: 'Tatooine',
+    d: 'Yavin',
+    answer: 'ans1'
+}, {
+    question: "Who did the cat in 'The Godfather' belong to?",
+    a: 'Francis Ford Coppola',
+    b: 'Diane Keaton',
+    c: 'Al Pachino',
+    d: 'No one—the cat was a stray',
+    answer: 'ans4'
+}, {
+    question: "What score did Elle Woods get on her LSAT in Legally Blonde?",
+    a: '155',
+    b: '166',
+    c: '179',
+    d: "182",
+    answer: 'ans3'
+}, {
+    question: "Who directed the hit 2017 movie 'Get Out'?",
+    a: 'James Wan',
+    b: 'Jordan Peele',
+    c: 'Guillermo del Toro',
+    d: 'Tim Story',
+    answer: 'ans2'
+}, {
+    question: "Which is the first movie in the 'Austin Powers' franchise?",
+    a: 'Austin Powers: Spy of the World ',
+    b: 'Austin Powers: The Spy Who Shagged Me',
+    c: 'Austin Powers: International Man of Mystery',
+    d: 'Austin Powers in Goldmember ',
+    answer: 'ans3'
+}, {
+    question: "Which two movies started from the same script?",
+    a: 'Star Wars and Close Encounters of the Third Kind',
+    b: 'E.T. and Poltergeist',
+    c: 'The Goonies and Indiana Jones',
+    d: 'Jurassic Park and The Land Before Time',
+    answer: 'ans2'
+}, {
+    question: "Which movie was not directed by Tim Burton?",
+    a: 'The Witches',
+    b: 'Pee-Wee’s Big Adventure',
+    c: 'Corpse Bride',
+    d: 'Big Fish',
+    answer: 'ans1'
+}, {
+    question: "Which movie does not feature Emma Stone?",
+    a: 'Superbad',
+    b: 'Easy A',
+    c: 'The Help',
+    d: 'No Strings Attached',
+    answer: 'ans4'
+}, {
+    question: "What object was Toy Story’s Woody originally?",
+    a: 'A ventriloquist dummy',
+    b: 'A puppet',
+    c: 'A clown doll',
+    d: 'A nesting doll',
+    answer: 'ans1'
+}, {
+    question: "Who directed Silence of the Lambs?",
+    a: 'Wes Anderson',
+    b: 'Jonathan Demme',
+    c: 'Oliver Stone',
+    d: 'Christopher Nolan',
+    answer: 'ans2'
+}, {
+    question: "Which isn’t a core rule for handling a Mogwai in 'Gremlins'?",
+    a: 'Do not feed it after midnight',
+    b: 'Do not expose it to bright lights',
+    c: 'Do not let it get cold',
+    d: 'Do not let it get wet',
+    answer: 'ans3'
+}, {
+    question: "Which professional athlete was considered for the lead in 'The Terminator'?",
+    a: 'Dan Marino',
+    b: 'Mike Tyson',
+    c: 'O.J. Simpson',
+    d: 'Wayne Gretzky',
+    answer: 'ans3'
+}, {
+    question: "How old was the voice actress of Monster Inc.’s Boo at the time of production?",
+    a: 'Four',
+    b: 'Two and a half',
+    c: 'Six',
+    d: "Ten",
+    answer: 'ans2'
+}, {
+    question: "Who took home an Oscar for Best Actor in 1998?",
+    a: 'Matt Damon',
+    b: 'Jack Nicholson',
+    c: 'Liam Neeson',
+    d: 'Sean Penn',
+    answer: 'ans2'
+}, {
+    question: "A Harry Potter stunt double was paralyzed on the job. Which actor did the stunt double do stunts for?",
+    a: 'Emma Watson',
+    b: 'Rupert Grint',
+    c: 'Alan Rickman',
+    d: 'Daniel Radcliffe',
+    answer: 'ans4'
+}, {
+    question: "'Dracula' was filmed in English during the day, and in what other language at night?",
+    a: 'German',
+    b: 'Sweedish',
+    c: 'Spanish',
+    d: 'French',
+    answer: 'ans3'
+}, {
+    question: "What year was the first 'Die Hard' movie released?",
+    a: '1986',
+    b: '1988',
+    c: '1990',
+    d: '1991',
     answer: 'ans2'
 }, ];
